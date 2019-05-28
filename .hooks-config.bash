dcc6ec4b91190cbaa9777c3f052b5a1331bfd5c3
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-05-28 16:09:57 -0400

    BUG: Fix pre-commit hook path
    Fix pre-commit hook path.

--- .hooks-config.bash
+++ .hooks-config.bash
@@ -1,4 +1,4 @@
 # Loaded by .git/hooks/(pre-commit|commit-msg|prepare-commit-msg)
 # during git commit after local hooks have been installed.
 
-hooks_chain_pre_commit="Utils/hooks/pre-commit"
+hooks_chain_pre_commit="utils/hooks/pre-commit"
